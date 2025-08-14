8fd723528c0465de5a22711a2fd121041fa0d9b1
COMLAB5-42 <Sean Girado>
2025-08-14 14:37:58 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+ 
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello I am Sean Kurtley J. Girado. I am a third year student student at the University of the Assumption, taking the course of Bachelor of science in Information Technology. Pangarap kong maging katulad si sir!</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
